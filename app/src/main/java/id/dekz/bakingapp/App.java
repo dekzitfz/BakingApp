19dedd7859f263602ddb39f1e937056a6f3de692
Adi Andrea <dekz.itfz@gmail.com>
2017-06-27 20:15:53 +0800

    init leak canary on debug

--- app/src/main/java/id/dekz/bakingapp/App.java
+++ app/src/main/java/id/dekz/bakingapp/App.java
@@ -2,6 +2,8 @@ package id.dekz.bakingapp;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import id.dekz.bakingapp.util.rest.RestClient;
 
 /**
@@ -15,6 +17,14 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if(BuildConfig.BUILD_TYPE.equals("debug")){
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                return;
+            }
+            LeakCanary.install(this);
+        }
+
         restClient = new RestClient();
     }
 
