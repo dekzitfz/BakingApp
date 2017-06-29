67ce77e66510f4609c488732597ac3834d64eaff
Adi Andrea <dekz.itfz@gmail.com>
2017-06-29 14:53:43 +0800

    add toolbar

--- app/src/main/java/id/dekz/bakingapp/features/recipelist/RecipeListActivity.java
+++ app/src/main/java/id/dekz/bakingapp/features/recipelist/RecipeListActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.List;
 
@@ -19,12 +20,15 @@ public class RecipeListActivity extends AppCompatActivity implements RecipeListV
     private RecipeAdapter adapter;
 
     @BindView(R.id.rv_recipe)RecyclerView rv;
+    @BindView(R.id.toolbar)Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
 
         setupRecyclerView();
         onAttachView();
