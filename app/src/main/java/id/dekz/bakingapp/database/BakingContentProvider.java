405852f68bec0adeb6393b57f4c349538093e83c
Adi Andrea <dekz.itfz@gmail.com>
2017-06-30 12:17:03 +0800

    impl insert ingredients and steps

--- app/src/main/java/id/dekz/bakingapp/database/BakingContentProvider.java
+++ app/src/main/java/id/dekz/bakingapp/database/BakingContentProvider.java
@@ -6,12 +6,15 @@ import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+
+import static id.dekz.bakingapp.database.contract.StepContract.StepEntry;
+
+import static id.dekz.bakingapp.database.contract.IngredientContract.IngredientEntry;
 
 import static id.dekz.bakingapp.database.contract.RecipeContract.RecipeEntry;
 
@@ -30,7 +33,9 @@ public class BakingContentProvider extends ContentProvider {
 
     private static final int RECIPES = 100;
     private static final int RECIPE_WITH_ID = 101;
+    private static final int INGREDIENTS = 200;
     private static final int INGREDIENTS_WITH_RECIPE_ID = 201;
+    private static final int STEPS = 300;
     private static final int STEPS_WITH_RECIPE_ID = 301;
     private static final UriMatcher uriMatcher = buildUriMatcher();
 
@@ -40,7 +45,9 @@ public class BakingContentProvider extends ContentProvider {
         UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
         matcher.addURI(AUTHORITY, PATH_RECIPES, RECIPES);
         matcher.addURI(AUTHORITY, PATH_RECIPES + "/#", RECIPE_WITH_ID);
+        matcher.addURI(AUTHORITY, PATH_INGREDIENTS, INGREDIENTS);
         matcher.addURI(AUTHORITY, PATH_INGREDIENTS + "/#", INGREDIENTS_WITH_RECIPE_ID);
+        matcher.addURI(AUTHORITY, PATH_STEPS, STEPS);
         matcher.addURI(AUTHORITY, PATH_STEPS + "/#", STEPS_WITH_RECIPE_ID);
 
         return matcher;
@@ -64,22 +71,41 @@ public class BakingContentProvider extends ContentProvider {
         return null;
     }
 
+    @SuppressWarnings("ConstantConditions")
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
         Uri result = null;
+        long id;
 
         final SQLiteDatabase db = dbHelper.getWritableDatabase();
         int match = uriMatcher.match(uri);
         switch (match){
             case RECIPES:
-                long id = db.insert(RecipeEntry.TABLE_NAME, null, values);
+                id = db.insert(RecipeEntry.TABLE_NAME, null, values);
                 if(id > 0){
                     result = ContentUris.withAppendedId(uri, id);
-                    //noinspection ConstantConditions
                     getContext().getContentResolver().notifyChange(uri, null);
                 }else{
                     Log.w(TAG, "inserting recipe data was failed!");
+                }
+                break;
+            case INGREDIENTS:
+                id = db.insert(IngredientEntry.TABLE_NAME, null, values);
+                if(id > 0){
+                    result = ContentUris.withAppendedId(uri, id);
+                    getContext().getContentResolver().notifyChange(uri, null);
+                }else{
+                    Log.w(TAG, "inserting ingredient data was failed!");
+                }
+                break;
+            case STEPS:
+                id = db.insert(StepEntry.TABLE_NAME, null, values);
+                if(id > 0){
+                    result = ContentUris.withAppendedId(uri, id);
+                    getContext().getContentResolver().notifyChange(uri, null);
+                }else{
+                    Log.w(TAG, "inserting step data was failed!");
                 }
                 break;
             default:
--- app/src/main/java/id/dekz/bakingapp/features/recipelist/RecipeListPresenter.java
+++ app/src/main/java/id/dekz/bakingapp/features/recipelist/RecipeListPresenter.java
@@ -9,7 +9,13 @@ import java.util.List;
 import id.dekz.bakingapp.App;
 import id.dekz.bakingapp.basemvp.BasePresenter;
 import static id.dekz.bakingapp.database.contract.RecipeContract.RecipeEntry;
+
+import static id.dekz.bakingapp.database.contract.IngredientContract.IngredientEntry;
+
+import static id.dekz.bakingapp.database.contract.StepContract.StepEntry;
+import id.dekz.bakingapp.model.Ingredient;
 import id.dekz.bakingapp.model.Recipe;
+import id.dekz.bakingapp.model.Step;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -49,7 +55,7 @@ public class RecipeListPresenter implements BasePresenter<RecipeListView> {
                     if(response.body() != null){
                         view.onDataReceived(response.body());
                         for(Recipe r : response.body()){
-                            saveData(r);
+                            saveRecipeData(r);
                         }
                     }else{
                         Log.w(TAG, "response body is null!");
@@ -66,13 +72,43 @@ public class RecipeListPresenter implements BasePresenter<RecipeListView> {
         });
     }
 
-    void saveData(Recipe recipe){
-        ContentValues values = new ContentValues();
-        values.put(RecipeEntry.RECIPE_ID, recipe.getId());
-        values.put(RecipeEntry.RECIPE_NAME, recipe.getName());
-        values.put(RecipeEntry.RECIPE_IMAGE, recipe.getImage());
-        values.put(RecipeEntry.RECIPE_SERVINGS, recipe.getServings());
+    private void saveRecipeData(Recipe recipe){
+        ContentValues recipeValues = new ContentValues();
+        recipeValues.put(RecipeEntry.RECIPE_ID, recipe.getId());
+        recipeValues.put(RecipeEntry.RECIPE_NAME, recipe.getName());
+        recipeValues.put(RecipeEntry.RECIPE_IMAGE, recipe.getImage());
+        recipeValues.put(RecipeEntry.RECIPE_SERVINGS, recipe.getServings());
 
-        view.getResolver().insert(RecipeEntry.CONTENT_URI, values);
+        view.getResolver().insert(RecipeEntry.CONTENT_URI, recipeValues);
+
+        for(Ingredient i : recipe.getIngredients()){
+            saveIngredientData(i, recipe.getId());
+        }
+
+        for(Step s : recipe.getSteps()){
+            saveStepData(s, recipe.getId());
+        }
+    }
+
+    private void saveIngredientData(Ingredient ingredient, int recipeID){
+        ContentValues ingredientValues = new ContentValues();
+        ingredientValues.put(IngredientEntry.RECIPE_ID, recipeID);
+        ingredientValues.put(IngredientEntry.INGREDIENT_QUANTITY, ingredient.getQuantity());
+        ingredientValues.put(IngredientEntry.INGREDIENT_MEASURE, ingredient.getMeasure());
+        ingredientValues.put(IngredientEntry.INGREDIENT, ingredient.getIngredient());
+
+        view.getResolver().insert(IngredientEntry.CONTENT_URI, ingredientValues);
+    }
+
+    private void saveStepData(Step step, int recipeID){
+        ContentValues stepValues = new ContentValues();
+        stepValues.put(StepEntry.RECIPE_ID, recipeID);
+        stepValues.put(StepEntry.STEP_ID, step.getId());
+        stepValues.put(StepEntry.STEP_DESCRIPTION, step.getDescription());
+        stepValues.put(StepEntry.STEP_SHORT_DESC, step.getShortDescription());
+        stepValues.put(StepEntry.STEP_THUMBNAIL_URL, step.getThumbnailURL());
+        stepValues.put(StepEntry.STEP_VIDEO_URL, step.getVideoURL());
+
+        view.getResolver().insert(StepEntry.CONTENT_URI, stepValues);
     }
 }
