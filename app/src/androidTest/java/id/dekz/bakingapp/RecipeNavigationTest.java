648b562eff3f46cf870bd159c42505a9b42b48ae
Adi Andrea <dekz.itfz@gmail.com>
2017-07-16 13:54:33 +0800

    init test nav from recipe list to detail recipe

---
+++ app/src/androidTest/java/id/dekz/bakingapp/RecipeNavigationTest.java
@@ -0,0 +1,73 @@
+package id.dekz.bakingapp;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import id.dekz.bakingapp.features.recipelist.RecipeListActivity;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+
+/**
+ * Created by DEKZ on 7/16/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeNavigationTest {
+
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
+    }
+
+    @Rule
+    public ActivityTestRule<RecipeListActivity> testRule =
+            new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Test
+    public void selectRecipe_CheckIfToolbarDisplayedCorrectRecipe(){
+        //pass an int recipe position here
+        testScenarioCheckToolbarTitle(3);
+    }
+
+    private void testScenarioCheckToolbarTitle(int position){
+        //scroll to item position
+        onView(withId(R.id.rv_recipe))
+                .perform(scrollToPosition(position));
+
+        //click item
+        onView(withRecyclerView(R.id.rv_recipe).atPosition(position))
+                .perform(click());
+
+        //check if toolbar title contain the correct recipe name
+        onView(withId(R.id.toolbar))
+                .check(matches(hasDescendant(withText(getRecipeName(position)))));
+    }
+
+    private String getRecipeName(int position){
+        switch (position){
+            case 0:
+                return "Nutella Pie";
+            case 1:
+                return "Brownies";
+            case 2:
+                return "Yellow Cake";
+            case 3:
+                return "Cheesecake";
+            default:
+                return "";
+        }
+    }
+
+}
