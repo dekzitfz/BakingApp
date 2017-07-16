067bf18b90ed791316f1e2bfc7595bd89c85b607
Adi Andrea <dekz.itfz@gmail.com>
2017-07-16 12:37:37 +0800

    write test for recyclerview

---
+++ app/src/androidTest/java/id/dekz/bakingapp/RecipeListTest.java
@@ -0,0 +1,39 @@
+package id.dekz.bakingapp;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import id.dekz.bakingapp.features.recipelist.RecipeListActivity;
+
+/**
+ * Created by DEKZ on 7/16/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeListTest {
+
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
+    }
+
+    @Rule
+    public ActivityTestRule<RecipeListActivity> testRule =
+            new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Test
+    public void checkRecipePosition(){
+        Espresso.onView(withRecyclerView(R.id.rv_recipe).atPosition(1))
+                .check(ViewAssertions.matches(
+                        ViewMatchers.hasDescendant(
+                                ViewMatchers.withText("Brownies"))
+                        )
+                );
+    }
+}
