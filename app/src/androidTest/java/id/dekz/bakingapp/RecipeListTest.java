a27d57f6e8dd3bfe1f36a1b9726d19888f8517fa
Adi Andrea <dekz.itfz@gmail.com>
2017-07-17 12:39:54 +0800

    update test

--- app/src/androidTest/java/id/dekz/bakingapp/RecipeListTest.java
+++ app/src/androidTest/java/id/dekz/bakingapp/RecipeListTest.java
@@ -1,11 +1,14 @@
 package id.dekz.bakingapp;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,9 +26,22 @@ public class RecipeListTest {
         return new RecyclerViewMatcher(recyclerViewId);
     }
 
+    private IdlingResource idlingResource;
+
     @Rule
     public ActivityTestRule<RecipeListActivity> testRule =
             new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Before
+    public void registerIdlingResource(){
+        idlingResource = testRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(idlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResource(){
+        if(idlingResource != null) Espresso.unregisterIdlingResources(idlingResource);
+    }
 
     @Test
     public void checkRecipePositionContainCorrectText(){
--- app/src/androidTest/java/id/dekz/bakingapp/RecipeNavigationTest.java
+++ app/src/androidTest/java/id/dekz/bakingapp/RecipeNavigationTest.java
@@ -1,9 +1,12 @@
 package id.dekz.bakingapp;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,10 +33,22 @@ public class RecipeNavigationTest {
     public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
         return new RecyclerViewMatcher(recyclerViewId);
     }
+    private IdlingResource idlingResource;
 
     @Rule
     public ActivityTestRule<RecipeListActivity> testRule =
             new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Before
+    public void registerIdlingResource(){
+        idlingResource = testRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(idlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResource(){
+        if(idlingResource != null) Espresso.unregisterIdlingResources(idlingResource);
+    }
 
     @Test
     public void selectRecipe_CheckIfToolbarDisplayedCorrectRecipe(){
